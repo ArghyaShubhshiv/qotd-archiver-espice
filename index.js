a6745bc85c42b180fa18993bf1175ad0cb08c3e7
ArghyaShubhshiv <57139099+ArghyaShubhshiv@users.noreply.github.com>
2020-07-07 11:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const fetch = require("node-fetch");
+const fs = require("fs");
 
 const client = new Discord.Client();
 const token = process.env.TOKEN;
